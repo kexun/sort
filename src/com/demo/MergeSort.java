193d88c5ad79a581b226c922e8a251e732bbeb0a
kexun <290464784@qq.com>
2016-07-16 16:12:34 +0800

    归并排序 非递归调用方法

--- src/com/demo/MergeSort.java
+++ src/com/demo/MergeSort.java
@@ -9,14 +9,14 @@ public class MergeSort {
 
 	public static void main(String[] args) {
 		int[] data = {
-				50,10,90,30,70,40,80,60,20
+				5,2
 			};
 		
-		int[] dest = new int[9];
+		int[] dest = new int[data.length];
 		
 		MergeSort m = new MergeSort();
-		m.sort(data, data);
-		for (int d : data) {
+		m.sort(data, dest);
+		for (int d : dest) {
 			System.out.println(d);
 		}
 
@@ -30,11 +30,12 @@ public class MergeSort {
 			return;
 		}
 		
-		mergeSort(data, dest, 0, length-1);
+//		mergeSort(data, dest, 0, length-1);
 		
+		mergeSort2(data, dest, length);
 	}
 
-
+	// 递归调用
 	private void mergeSort(int[] data, int[] dest, int s, int e) {
 		
 		if (s == e) {
@@ -49,7 +50,6 @@ public class MergeSort {
 		merge(to, dest, s, m, e);
 		
 	}
-
 
 	private void merge(int[] data, int[] dest, int s, int m, int e) {
 
@@ -73,4 +73,62 @@ public class MergeSort {
 		}
 	}
 	
+	///////////////////////////////////////////////////////////////////////////
+	
+	// 第二种方式，非递归方式排序, 特别要注意这里的一些边界情况
+	// 这里用到了两个数组 data和dest，这两个数组在merge的时候相互调换使用，
+	// 第一次是data->dest  第二次dest->data 但是最后一次必须确保是data->dest
+	// 因此就出现了
+	//			  if (k > length) {
+	//   				System.arraycopy(data, 0, dest, 0, length);
+	// 			  }
+	// 这个数组拷贝是为了防止，最后一次数据合并没有发生在dest上 而是data上， 所以讲data的数据拷贝
+	// 到dest上去。
+	private void mergeSort2(int[] data, int[] dest, int length) {
+		
+		int k = 1;
+		while (k <= length) {
+			mergePass(data, dest, k, length);
+			k*=2;
+			mergePass(dest, data, k, length);
+			k*=2;
+			if (k > length) {
+				System.arraycopy(data, 0, dest, 0, length);
+			}
+		}
+		
+	}
+	
+	private void mergePass(int[] data, int[] dest, int k, int length) {
+		
+		int i = 0;
+		while (i <= length-2*k) {
+			merge(data, dest, i, i+k-1, i+2*k-1);
+			i = i + 2*k;
+		}
+		
+		if ((length-i) > k) {
+			merge(data, dest, i, i+k-1, length-1);
+		} else {
+			for (int j = i; j < length; j++) {
+				dest[j] = data[j];
+			}
+		}
+		
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
