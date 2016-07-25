45ce7d02ebc357a32ddbcaad1623b29ce9c29a09
kexun <qianchj@163.com>
2016-07-25 22:43:05 +0800

    test

--- src/com/demo/LISA2.java
+++ src/com/demo/LISA2.java
@@ -2,11 +2,13 @@ package com.demo;
 
 /**
  * 给定一个长度为N的数组，找出一个最长的单调自增子序列（不一定连续，但是顺序不能乱）
-例如：给定一个长度为8的数组A{1,3,5,2,4,6,7,8}，则其最长的单调递增子序列为{1,2,4,6,7,8}，长度为6.
+ * 例如：给定一个长度为8的数组A{1,3,5,2,4,6,7,8}，则其最长的单调递增子序列为{1,2,4,6,7,8}，长度为6.
+ * 
+ * 没有通过全部测试用例，可能问题是。如果存在多个最长子序列，应该打印第一个把。
  */
 import java.util.Scanner;
 
-public class LISA {
+public class LISA2 {
 
 	public static void main(String[] args) {
 
@@ -58,7 +60,21 @@ public class LISA {
 				max = k;
 			}
 		}
-		System.out.println(max);
+		
+		int[] out = new int[max];
+		for (int k=values.length-1; k>=0; k--) {
+			
+			if (values[k] == max) {
+				out[--max] = array[k];
+				if (max == 0) {
+					break;
+				}
+			}
+		}
+		
+		for (int o : out) {
+			System.out.print(o+" ");
+		}
 	
 	}
 
