fed178c037a92f4a7b77d6949f925576b8d9c624
kexun <qianchj@163.com>
2016-08-27 23:29:39 +0800

    优化代码，提高运行效率

--- src/com/demo/MaxPointsNoLine.java
+++ src/com/demo/MaxPointsNoLine.java
@@ -1,16 +1,13 @@
 package com.demo;
 
-import java.math.BigDecimal;
 import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Iterator;
 import java.util.Map;
 import java.util.Map.Entry;
-import java.util.Set;
 
 /**
  * Given n points on a 2D plane, find the maximum number 
  * of points that lie on the same straight line.
+ * 优化了部分代码，提高运行效率
  * @author kexun
  *
  */
@@ -35,68 +32,51 @@ public class MaxPointsNoLine {
 	
 	public int maxPoints(Point[] points) {
 
-		Map<Line, Set<MyPoint>> map = new HashMap<Line, Set<MyPoint>>();
+		Map<Double, Integer> map = new HashMap<Double, Integer>();
 		int max = 0;
-		MyPoint[] myPoints = new MyPoint[points.length];
-		
-		for (int i=0; i<points.length; i++) {
+
+		int length = points.length;
+		if(length <= 2)
+			return length;
+
+		for (int i=0; i<length; i++) {
 			
 			Point p1 = points[i];
-			if (p1 == null) {
-				continue;
-			}
-			MyPoint mp1 = myPoints[i];
-			if (mp1 == null) {
-				mp1 = new MyPoint(p1);
-				myPoints[i] = mp1;
+			int samePoint = 1;
+			int yp = 1;
+			for (int j=i+1; j<length; j++) {
+				Point p2 = points[j];
+				
+				if (p1.x == p2.x && p1.y == p2.y) {
+					samePoint++;
+				} else if (p1.x == p2.x) {
+					yp++;
+				} else {
+					double k = calLine(p1, p2);
+					Integer temp = map.get(k);
+					if (temp == null) {
+						temp = 1;
+					} else {
+						temp++;
+					}
+					map.put(k, temp);
+				}
 			}
 			
-			Set<MyPoint> set = new HashSet<MyPoint>();
-			for (int j=i+1; j<points.length; j++) {
-
-				Point p2 = points[j];
-				if (p2 == null) {
-					continue;
-				}
-				
-				MyPoint mp2 = myPoints[j];
-				if (mp2 == null) {
-					mp2 = new MyPoint(p2);	
-					myPoints[j] = mp2;
-				}
-				
-				if (!isSamePoint(p1, p2)) {
-					Line line = calLine(p1, p2);
-					set = map.get(line);
-					if (set == null) {
-						set = new HashSet<MyPoint>();
-						map.put(line, set);
-					}
-					set.add(mp1);
-					set.add(mp2);
-				} else {
-					mp1.add();
-					points[j] = null;
-					myPoints[j] = null;
-				}
+			for(Entry<Double, Integer> e : map.entrySet()) {
+				int tempMax = e.getValue()+samePoint;
+				if (tempMax > max)
+					max = tempMax;
 			}
+			if (yp > max) {
+				max = yp;
+			}
+			if (samePoint > max) {
+				max = samePoint;
+			}
+			map.clear();
 		}
 		
-		Iterator<Entry<Line, Set<MyPoint>>> it = map.entrySet().iterator();
-		while (it.hasNext()) {
-			Set<MyPoint> list = it.next().getValue();
-			int count = 0;
-			for (MyPoint p : list) {
-				count += p.count;
-			}
-			if (count > max) {
-				max = count;
-			}
-		}
-		
-		if (max == 0) {
-			return points.length;
-		}
 		return max;
     }
 	
@@ -104,22 +84,17 @@ public class MaxPointsNoLine {
 		return p1.x == p2.x && p1.y == p2.y;
 	}
 	
-	public Line calLine(Point p1, Point p2) {
+	public double calLine(Point p1, Point p2) {
 
 		double k = Double.MAX_VALUE;
-		double b = 0;
 		if (p2.x != p1.x) {
 			k = (double) (1.0*(p2.y - p1.y)/(p2.x - p1.x));
 		}
 		
-		if (k == 0) {
-			b = p2.y;
-		} else if (k == Double.MAX_VALUE) {
-			b = p2.x;
-		} else {
-			b = p2.y - k*p2.x;
+		if (Math.abs(k) == 0) {
+			k = 0;
 		}
-		return new Line(k, b);
+		return k;
 	}
 
 	class Point {
@@ -138,45 +113,4 @@ public class MaxPointsNoLine {
 		
 	}
 	
-	class MyPoint {
-		
-		Point p;
-		int count = 1;
-		
-		public MyPoint(Point p) {
-			this.p = p;
-		}
-		
-		public void add() {
-			count++;
-		}
-		
-		public boolean equals(Point obj) {
-			if (obj == null)
-				return false;
-			return p.x == obj.x && p.y == obj.y;
-		}
-	}
-	
-	public class Line {
-		public double k;
-		public double b;
-		
-		Line(double k, double b) {
-			this.k =  new BigDecimal(k).setScale(4, BigDecimal.ROUND_DOWN).doubleValue();
-			this.b = new BigDecimal(b).setScale(4, BigDecimal.ROUND_DOWN).doubleValue();
-		}
-
-		@Override
-		public boolean equals(Object obj) {
-			boolean a = this.k == ((Line)obj).k && this.b == ((Line)obj).b;
-			return a;
-		}
-		
-		@Override
-		public int hashCode() {
-			return 1;
-		}
-	}
-	 
 }
